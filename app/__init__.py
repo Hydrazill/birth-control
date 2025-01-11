84514686798f2c803c5db9ae1238149da67b8f45
hydrazill <99666026+Hydrazill@users.noreply.github.com>
2025-01-11 15:49:07 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ logging.basicConfig(level=logging.DEBUG)
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = 'bab596d6d77abcd51fc5cdd1a3856e81749f225d'
-    app.config["MONGO_URI"] = "mongodb://mongo:OZaPicRClcWHfwKTMIIOqAOGhMumMQvY@junction.proxy.rlwy.net:29950" # "mongodb+srv://l0rd_9h057:4NSZCNQDRyGi2IEw@birthcontrolcluster.bzeks.mongodb.net/birthControl?retryWrites=true&w=majority" # "mongodb+srv://l0rd_9h057:4NSZCNQDRyGi2IEw@birthcontrolcluster.bzeks.mongodb.net/?retryWrites=true&w=majority&appName=birthControlCluster"
+    app.config["MONGO_URI"] = "mongodb+srv://l0rd_9h057:4NSZCNQDRyGi2IEw@birthcontrolcluster.bzeks.mongodb.net/birthControl?retryWrites=true&w=majority" # "mongodb+srv://l0rd_9h057:4NSZCNQDRyGi2IEw@birthcontrolcluster.bzeks.mongodb.net/?retryWrites=true&w=majority&appName=birthControlCluster"
 
     mongodb_client = PyMongo(app)
     db = mongodb_client.db
@@ -24,8 +24,8 @@ def create_app():
         db='birthControl',  # replace with your database name
         host=app.config["MONGO_URI"],         # replace with your MongoDB host
         port=27017,               # replace with your MongoDB port if different
-        username='mongo', # 'l0rd_9h057', # add this if authentication is required
-        password='OZaPicRClcWHfwKTMIIOqAOGhMumMQvY', # '4NSZCNQDRyGi2IEw', # add this if authentication is required
+        username='l0rd_9h057', # add this if authentication is required
+        password='4NSZCNQDRyGi2IEw', # add this if authentication is required
         alias='default'           # alias for the connection
     )
 
